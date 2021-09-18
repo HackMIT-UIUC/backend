c5de57fbdc7a9c4e2930ab647bf407ea3f0cda60
Schwidola0607 <khoadangpham82944@gmail.com>
2021-09-18 15:17:11 -0500

    create request

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,11 +1,35 @@
-const express = require('express')
+const express = require('express');
+const {check, validationResult} = require('express-validator');
 const router = express.Router();
-
-// @route  GET api/post
-// @desc   Test route
-// @access Public
+const Post = require('../../models/Posts')
+// @route  POST api/post
+// @desc   Create a post
+// @access Private
 
 //CRUD stuff here
-router.get('/', (req, res) => res.send('Post route'));
 
+router.post('/', [
+  check('message', 'post content is required').not().isEmpty(),
+],
+(req, res) => {
+    const error = validationResult(req);
+    if(!error.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+        const newPost = new Posts({
+            postID: req.body["postID"],
+            username: req.body["username"],
+            message: req.body["message"],
+            symptomps: req.body["symptomps"],
+        })
+        const post = newPost.save();
+        res.json(post);
+        console.log(req.body);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+})
+//
 module.exports = router;
