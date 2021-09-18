ab753299638728d5fe58f672ccbea0a572eecba3
Kunal Sahni <kunal.sahni@mail.polimi.it>
2021-09-18 14:05:34 -0500

    Implemented read and delete

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,5 +1,6 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router();
+const Post = require("./models/Posts");
 
 // @route  GET api/post
 // @desc   Test route
@@ -7,5 +8,44 @@ const router = express.Router();
 
 //CRUD stuff here
 router.get('/', (req, res) => res.send('Post route'));
+
+// @route  GET api/post
+// @desc   Retrieve post by its id
+// @access Public
+router.get("/post/:id", async (req, res) => {
+	try {
+		const post = await Post.findOne({ postID: req.params.id })
+		res.send(post)
+	} catch {
+		res.status(404)
+		res.send({ error: "Post doesn't exist!" })
+	}
+})
+
+// @route  GET api/post
+// @desc   Retrieve all posts by a user
+// @access Public
+router.get("/posts/:username", async (req, res) => {
+	try {
+		const posts = await Post.find({ username: req.params.username })
+		res.send(posts)
+	} catch {
+		res.status(404)
+		res.send({ error: "User has no posts!" })
+	}
+})
+
+// @route  GET api/post
+// @desc   delete post based on the id
+// @access Public
+router.get("/delete/:id", async (req, res) => {
+	try {
+		const post = await Post.findOneAndDelete({ postID: req.params.id })
+		res.send(post)
+	} catch {
+		res.status(404)
+		res.send({ error: "Post doesn't exist!" })
+	}
+})
 
 module.exports = router;
