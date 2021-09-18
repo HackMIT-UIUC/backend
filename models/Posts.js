7ddbf869fd12c1bf21322bff0124367419f32683
Kunal Sahni <sahnikunalk13@gmail.com>
2021-09-18 13:11:20 -0500

    Added schema

--- models/Posts.js
+++ models/Posts.js
@@ -1,2 +1,30 @@
 const mongoose = require('mongoose');
 const Schema = require.Schema;
+
+const postSchema = new Schema({
+    postID: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        validate: [validateEmail, 'Please fill a valid email address'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+    message: {
+        type: String,
+        minLength: 50,
+        required: true
+    },
+    symptoms: {
+        type: Array,
+        required: true,
+        validate : {
+            validator : function(array) {
+              return arrazy.length > 0 && array.every((v) => typeof v === 'string');
+            }
+        }
+    }
+})
